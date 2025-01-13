8000ef696e5327a1d0636b3ae73e35fe9a785380
Saima-Techpro <132275409+Saima-Techpro@users.noreply.github.com>
2025-01-14 01:28:10 +0400

    Update Q01_Actions_Facebook.java

--- src/test/java/practice08/Q01_Actions_Facebook.java
+++ src/test/java/practice08/Q01_Actions_Facebook.java
@@ -1,12 +1,18 @@
 package practice08;
 
 import com.github.javafaker.Faker;
+import com.myfirstseleniumproject.utilities.TestBase;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
-import utilities.TestBase;
 
-public class Q01_Actions_Facebook extends TestBase {
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class HW_Facebook extends TestBase {
     /*
         Go to https://www.facebook.com
         Click on "Create New Account"
@@ -18,71 +24,96 @@ public class Q01_Actions_Facebook extends TestBase {
     @Test
     public void facebookLoginTest() throws InterruptedException {
 
-//        Go to https://www.facebook.com
+        Faker faker = new Faker();
+
+        // Generate fake data
+        String firstName = faker.name().firstName();
+        String lastName = faker.name().lastName();
+        String email = faker.internet().emailAddress();
+        String password = faker.internet().password();
+
+        // Go to Facebook
         driver.get("https://www.facebook.com");
-        Thread.sleep(2000);
+        Thread.sleep(1000); // Ensure page has loaded
 
-//        Click on "Create New Account"
-//        Create Actions object
-
+        // Click "Create New Account"
         Actions actions = new Actions(driver);
-
         for (int i = 1; i < 5; i++) {
             actions.sendKeys(Keys.TAB).perform();
-            Thread.sleep(2000);
         }
-        Thread.sleep(2000);
+        // Wait for form to load
+        Thread.sleep(1000);
+        // Click on New Account
         actions.sendKeys(Keys.ENTER).perform();
+        Thread.sleep(2000); // Wait for the sign-up form to appear
 
-//        Fill all the boxes by using keyboard actions class
-        Faker faker = new Faker();
-        Thread.sleep(2000);
-        actions.sendKeys(faker.name().firstName()).perform();
-        Thread.sleep(2000);
+        // Click in first Name input
+        actions.sendKeys(Keys.TAB);
+        Thread.sleep(1000);
+
+        // Start filling the form with keyboard actions
+        actions.sendKeys(firstName) // First Name
+                .sendKeys("\t") // Tab to Last Name
+                .sendKeys(lastName) // Last Name
+                .sendKeys("\t")
+                .perform();
+
+        Thread.sleep(1000);
         actions.sendKeys(Keys.TAB).perform();
-        Thread.sleep(2000);
 
-        actions.sendKeys(faker.name().lastName()).perform();
-        Thread.sleep(2000);
+        // Select Birth Day using keyboard
+        actions.sendKeys(Keys.ARROW_DOWN).perform();
+        actions.sendKeys("8"); // Navigate to the day
+        Thread.sleep(1000);
+
+        actions.sendKeys(Keys.TAB).perform(); // Move to Birth Month
+
+        actions.sendKeys(Keys.ARROW_DOWN).perform();
+        actions.sendKeys("March");// Navigate to the month
+        Thread.sleep(1000);
+
+        actions.sendKeys(Keys.TAB).perform(); // Move to Birth Year
+        actions.sendKeys(Keys.ARROW_DOWN).perform();
+        actions.sendKeys("1990");// Navigate to the year
+        Thread.sleep(1000);
+
+        actions.sendKeys(Keys.TAB); // skip Gender question mark
+         // Move to Gender
+
+        actions.sendKeys(Keys.TAB) // Tab to the "Female" radio button
+                .sendKeys(Keys.SPACE) // Select Female radio button
+                .perform();
+
+
+        // Mobile number or email address
         actions.sendKeys(Keys.TAB).perform();
-        Thread.sleep(2000);
-
-        // create an email variable to send the same email again
-        String email = faker.internet().emailAddress();
+        Thread.sleep(1000);
         actions.sendKeys(email).perform();
-        actions.sendKeys(Keys.TAB).perform();
         Thread.sleep(1000);
 
-        // because email is asked twice
-        actions.sendKeys(email).perform();
-        actions.sendKeys(Keys.TAB).perform();
-        Thread.sleep(1000);
-
-        // Create password variable to send the same password
-        String password = faker.internet().password();
-        actions.sendKeys(password).perform();
-        actions.sendKeys(Keys.TAB).perform();
-        Thread.sleep(1000);
+        // sending password
+                actions.sendKeys("\t") // Tab to Password
+                .sendKeys(password)
+                .sendKeys("\t") // Tab to New Password
+                .sendKeys(password)
+                .perform();
 
 
-        actions.sendKeys(Keys.TAB).perform();
-        Thread.sleep(1000);
-        actions.sendKeys("12").
-                sendKeys(Keys.TAB).
-                sendKeys("Jul").
-                sendKeys(Keys.TAB).
-                sendKeys("2020").
-                sendKeys(Keys.TAB).
-                sendKeys(Keys.TAB).
-                //sendKeys(Keys.ARROW_RIGHT).
-                build().perform();
-
-
-        for (int i = 1; i < 6; i++) {
+        // Going to sign up button
+        for (int i = 0; i < 4; i++) {
             actions.sendKeys(Keys.TAB).perform();
         }
 
+        actions.sendKeys(Keys.ENTER).perform();
 
+
+        waitFor(15);
+
+        assertTrue(driver.getCurrentUrl().contains("confirmemail"));
 
     }
+
+
+
+
 }
